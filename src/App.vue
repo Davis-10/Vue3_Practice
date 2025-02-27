d65206e0187a17f1067e263aca321653b41d3708
Davis-10 <daviorbus@gmail.com>
2025-02-27 23:09:24 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import GameCounter from '@/components/GameCounter.vue'
+</script>
+
+<template>
+  <h1>Hoola Mundo</h1>
+
+  <GameCounter/>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/GameCounter.vue
@@ -0,0 +1,26 @@
+<script setup>
+const isActive = false
+</script>
+
+<template>
+  <button>menos</button>
+
+  <span class="counter" :class="{ 'counter--active': isActive }">contador: 0</span>
+
+  <button>mas</button>
+</template>
+
+<style scoped>
+.counter {
+  font-weight: bold;
+  font-size: 2rem;
+}
+
+.counter--active {
+  color: green;
+}
+
+button {
+  background-color: red;
+}
+</style>
